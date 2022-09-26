8ed7830bb4961e8ca8e65a1b8437b18b58032d6d
emerytown <ztown@outlook.com>
2022-09-26 07:50:11 -0700

    started ArrayListPractice

--- src/main/java/ArrayListPractice.java
+++ src/main/java/ArrayListPractice.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class ArrayListPractice {
     /**
      * Get the first element in the ArrayList.
-     * 
+     *
      * @param numbers List of numbers to search over.
      * @return The first element in the list.
      */
@@ -14,7 +14,7 @@ public class ArrayListPractice {
 
     /**
      * Get the third element in the ArrayList.
-     * 
+     *
      * @param numbers List of numbers to search over.
      * @return The third element in the list.
      */
@@ -25,55 +25,65 @@ public class ArrayListPractice {
 
     /**
      * Get the last element in the ArrayList.
-     * 
+     *
      * @param numbers List of numbers to search over.
      * @return The last element in the list.
      */
     public static int GetLast(ArrayList<Integer> numbers) {
-        return numbers.get(numbers.size()-1);
+        return numbers.get(numbers.size() - 1);
         // write your code above and remove the line below
     }
 
     /**
      * Get the sum of all elements in the ArrayList.
-     * 
+     *
      * @param numbers List of numbers to search over.
      * @return The sum of the elements in the list.
      */
     public static int GetSum(ArrayList<Integer> numbers) {
-        for(int i = 0; i < numbers.size(); i++){
-
+        int total = 0;
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            numbers.get(i);
+            total += numbers.get(i);
         }
+        return total;
         // write your code above and remove the line below
     }
 
     /**
      * Returns a string that contains all of the numbers in the ArrayList
      * separated by one space.
-     * 
+     * <p>
      * For example, if numbers = {1, 4, 3, 2, 6}, your output should
      * be "1 4 3 2 6".
-     * 
+     *
      * @param numbers List of numbers to search over.
      * @return A string representing the list.
      */
     public static String ConvertToString(ArrayList<Integer> numbers) {
-String numberList = "";
-        for(int i = 0; i <numbers.size() - 1; i++)
-            numberList+=numbers.get(i) + " ";
-        numberList+=numbers.get(numbers.size()-1); return numberList;
+        String numberList = "";
+        for (int i = 0; i < numbers.size() - 1; i++)
+            numberList += numbers.get(i) + " ";
+        numberList += numbers.get(numbers.size() - 1);
+        return numberList;
     }
 
     /**
      * Returns the largest element in the array
-     * 
+     *
      * @param numbers List of numbers to search over.
      * @return The largest element in the list.
      */
     public static int GetMax(ArrayList<Integer> numbers) {
-
+        int max = 0;
+        for (int i = 0; i < numbers.size() - 1; i++){
+            if(numbers.get(i)>numbers.get(i+1))
+                max = numbers.get(i);
+            else
+                max = numbers.get(i+1);
+        }
+        return max;
         // write your code above and remove the line below
-        throw new UnsupportedOperationException();
     }
 
     /**
@@ -81,30 +91,35 @@ String numberList = "";
      * Include the first element, but not the last one.
      * If the first and last elements are the same, then the list should be empty.
      * If the last number is less than the first, then the list should also be empty.
-     *
+     * <p>
      * For example:
-     *  CreateNumberArray(4, 8) returns {4, 5, 6, 7}
-     *  CreateNumberArray(2, 2) returns {} (an empty list)
-     * 
+     * CreateNumberArray(4, 8) returns {4, 5, 6, 7}
+     * CreateNumberArray(2, 2) returns {} (an empty list)
+     *
      * @param first The first number in the list (inclusive).
-     * @param last The last number in the list (exclusive).
+     * @param last  The last number in the list (exclusive).
      * @return A list of all numbers between first and last.
      */
     public static ArrayList<Integer> CreateNumberArray(int first, int last) {
-
+        ArrayList<Integer> nums = new ArrayList<>();
+        if(first == last)
+            return nums;
+        if (first > last)
+            return nums;
+        for(int i = first;i < last;i++)
+            nums.add(i);
+        return nums;
         // write your code above and remove the line below
-        throw new UnsupportedOperationException();
     }
-
     /**
      * Finds the first student whose favorite color is the one specified.
      *
      * @param students The list of students to search over.
-     * @param color The favorite color the student must have.
+     * @param color    The favorite color the student must have.
      * @return The first student whose favorite color is the one specified.
      */
     public static Student GetFirstStudentWithFavoriteColor(ArrayList<Student> students, String color) {
-
+        for(int i = )
         // write your code above and remove the line below
         throw new UnsupportedOperationException();
     }
@@ -113,7 +128,7 @@ String numberList = "";
      * Gets the favorite color for a specified student.
      *
      * @param students The list of students to search over.
-     * @param name The name of the student to look for.
+     * @param name     The name of the student to look for.
      * @return The favorite color of the specified student.
      */
     public static String GetFavoriteColorOfStudent(ArrayList<Student> students, String name) {
@@ -135,15 +150,15 @@ String numberList = "";
     }
 
     /**
-     *  Creates an array of Student objects using the information provided in via the parameters.
+     * Creates an array of Student objects using the information provided in via the parameters.
      * (names[0], heights[0] ...) are for the first student. (names[1], height[1]...)
      * are for the second student, etc.
-     * 
-     * @param names The list of names of the students.
-     * @param heights The list of heights of the students.
-     * @param gradeLevels The list of gradeLevels of the students.
+     *
+     * @param names          The list of names of the students.
+     * @param heights        The list of heights of the students.
+     * @param gradeLevels    The list of gradeLevels of the students.
      * @param favoriteColors The list of favoriteColors of the students.
-     * @param bankAccounts The list of bankAccounts of the students.
+     * @param bankAccounts   The list of bankAccounts of the students.
      * @return List of Student objects.
      */
     public static ArrayList<Student> CreateStudentArray(ArrayList<String> names, ArrayList<Integer> heights, ArrayList<Integer> gradeLevels, ArrayList<String> favoriteColors, ArrayList<BankAccount> bankAccounts) {
@@ -155,12 +170,12 @@ String numberList = "";
     /**
      * Returns a string that lists teams from a 2-D ArrayList. Each ArrayList in
      * the bigger ArrayList contains members of the team.
-     * 
+     * <p>
      * The expected output looks like this:
      * Team 1: Amy, Abby, Arnold
      * Team 2: Bob, Beatrice, Becky, Ben
      * Team 3: Claire
-     * 
+     *
      * @param teams The list of teams of students.
      * @return A string that lists the team number followed by each team.
      */
@@ -173,8 +188,8 @@ String numberList = "";
     /**
      * Finds the student with the given name, and updates their favorite color to the given color.
      *
-     * @param students The list of students to search over.
-     * @param name The name of the student whose favorite color should be updated.
+     * @param students         The list of students to search over.
+     * @param name             The name of the student whose favorite color should be updated.
      * @param newFavoriteColor The new favorite color of the student.
      */
     public static void UpdateFavoriteColor(ArrayList<Student> students, String name, String newFavoriteColor) {
@@ -186,7 +201,7 @@ String numberList = "";
     /**
      * Create an ArrayList consisting of all students in the given grade level.
      *
-     * @param students The list of students to search over.
+     * @param students   The list of students to search over.
      * @param gradeLevel The grade level the students should be in.
      * @return An ArrayList containing all the students in gradeLevel.
      */
@@ -199,12 +214,12 @@ String numberList = "";
     /**
      * Transfer money from one student to another.
      *
-     * @param students A list of students.
+     * @param students        A list of students.
      * @param fromStudentName The name of the student to transfer money from.
-     * @param toStudentName The name of the student to transfer money to.
-     * @param amount The amount of money to transfer.
+     * @param toStudentName   The name of the student to transfer money to.
+     * @param amount          The amount of money to transfer.
      * @return true if the transfer was successful, i.e. the student to transfer money from
-     *     had sufficient funds in their account. Otherwise, false.
+     * had sufficient funds in their account. Otherwise, false.
      */
     public static boolean TransferMoney(ArrayList<Student> students, String fromStudentName, String toStudentName, double amount) {
 
